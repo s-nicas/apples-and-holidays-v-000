9f82791f2b705f9f117648facc7306056287dbd2
Sonia  Nicas <sonia.d.nicas@gmail.com>
2018-04-26 16:36:29 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -19,36 +19,28 @@ def second_supply_for_fourth_of_july(holiday_hash)
   # }
   # return the second element in the 4th of July array
   holiday_hash[:summer][:fourth_of_july][1]
-
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-
    holiday_hash[:winter].collect do |holiday, decor|
      decor.push(supply)
    end
-
  end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
-
   holiday_hash[:spring].collect do |holiday, decor|
     decor.push(supply)
   end
-
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-
   holiday_hash[season][holiday_name] = supply_array
-
-
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  holiday_hash[:winter]
 end
 
 def all_supplies_in_holidays(holiday_hash)
