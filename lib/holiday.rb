ab94018f4cf2a0addae48075f1f1e6d319e00b28
Sonia  Nicas <sonia.d.nicas@gmail.com>
2018-04-26 16:24:44 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -36,11 +36,11 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
  #  end
  # end
 
- holiday_hash[:winter].each do |decor|
+   holiday_hash[:winter].collect do |season, decor|  
+     decor.push(supply)
+   end
    
-  decor.push(supply)
-end
-end
+ end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
