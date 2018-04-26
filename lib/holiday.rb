aa6c0c22c7b489ebb603a86d1b15e6ebc5104f99
Sonia  Nicas <sonia.d.nicas@gmail.com>
2018-04-26 19:06:46 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,7 +80,7 @@ def all_supplies_in_holidays(holiday_hash)
 
     day.each do |holiday, list|
 
-      list.each do |items|
+      list.each do |supplies|
         binding.pry 
         puts supplies
       end
